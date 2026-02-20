6aa66f94f226155b85f39315a4e61c9553d7a3d0
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-20 15:40:43 +0800

    replace View with ThemedView

--- app/about.jsx
+++ app/about.jsx
@@ -1,15 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { Link } from 'expo-router';
+
+import ThemedView from '../components/ThemedView';
 
 const About = () => {
   return (
-    <View style={styles.container}>
+    <ThemedView style={styles.container}>
       <Text style={styles.title}>About Page</Text>
 
       <Link href="/" style={styles.link}>
         Back Home
       </Link>
-    </View>
+    </ThemedView>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -3,9 +3,11 @@ import { Link } from 'expo-router';
 
 import Logo from '../assets/img/logo_light.png';
 
+import ThemedView from '../components/ThemedView';
+
 const Home = () => {
   return (
-    <View style={styles.container}>
+    <ThemedView style={styles.container}>
       <Image source={Logo} style={styles.img} />
 
       <Text style={styles.title}>The Number 1</Text>
@@ -17,7 +19,7 @@ const Home = () => {
       <Link href="/contact" style={styles.link}>
         Contact Page
       </Link>
-    </View>
+    </ThemedView>
   );
 };
 
