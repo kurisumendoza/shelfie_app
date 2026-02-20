9cbbf4e9749984a35389ee5cb02080e44f56a764
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-20 17:45:12 +0800

    refactor for safe area view

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,12 +1,30 @@
 import { View, useColorScheme } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Colors } from '../constants/Colors';
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe = false, ...props }) => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
+  if (!safe)
+    return (
+      <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    );
+
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    <View
+      style={[
+        {
+          backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        },
+        style,
+      ]}
+      {...props}
+    />
   );
 };
 
