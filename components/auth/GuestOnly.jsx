14eabdca6167a5ec3a2674b683da090a252f1eae
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-21 14:52:14 +0800

    add ThemedLoader component

--- components/auth/GuestOnly.jsx
+++ components/auth/GuestOnly.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { Text } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useUser } from '../../hooks/useUser';
+import ThemedLoader from '../ThemedLoader';
 
 const GuestOnly = ({ children }) => {
   const { user, authChecked } = useUser();
@@ -14,7 +15,7 @@ const GuestOnly = ({ children }) => {
   }, [user, authChecked]);
 
   if (!authChecked || user) {
-    return <Text>Loading</Text>;
+    return <ThemedLoader />;
   }
 
   return children;
