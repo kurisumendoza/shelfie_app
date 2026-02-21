8b3606a2ff0f18d5a04d88b391857459f3853c22
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-21 14:23:48 +0800

    prevent authenticated users to reach login and register pages

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,16 +1,17 @@
 import { StatusBar } from 'react-native';
 import { Stack } from 'expo-router';
 import { useUser } from '../../hooks/useUser';
+import GuestOnly from '../../components/auth/GuestOnly';
 
 const AuthLayout = () => {
   const { user } = useUser();
   console.log(user);
 
   return (
-    <>
+    <GuestOnly>
       <StatusBar style="auto" />
       <Stack screenOptions={{ headerShown: false, animation: 'none' }} />
-    </>
+    </GuestOnly>
   );
 };
 
